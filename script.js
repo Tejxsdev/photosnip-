b3cb94678c1d10f5aa922d25f9d5fbdd6f4c6803
Tejxsdev <danduhman7@gmail.com>
2025-02-27 16:41:49 -0400

    added camera functions

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
-const width = screen.width/2; 
-let height = 0; 
+const width = screen.width / 2;
+let height = 0;
+let counter = 0;
+let canvas = document.getElementById("snap");
+let photo = document.getElementById("photo");
 
 let streaming = false;
+document.getElementById("snap").style.display = "none";
+function isMobile() {
+  const regex = /Mobi|Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i;
+  return regex.test(navigator.userAgent);
+}
 
 let video = null;
 
 function startup() {
   video = document.getElementById("video");
   navigator.mediaDevices
-    .getUserMedia({ video: true, audio: false, facingMode: "user"})
+    .getUserMedia({ video: { facingMode: 'user' }, audio: false })
     .then((stream) => {
       video.srcObject = stream;
       video.play();
@@ -19,16 +27,61 @@ function startup() {
 
   video.addEventListener(
     "canplay",
-    function(e) {
+    function (e) {
       if (!streaming) {
-        height = video.videoHeight / (video.videoWidth / width);
-        video.set
+        if (isMobile()) {
+          height = width * (4 / 3);
+        } else {
+          height = video.videoHeight / (video.videoWidth / width);
+        }
         video.setAttribute("width", width);
         video.setAttribute("height", height);
+        canvas.setAttribute("width", width);
+        canvas.setAttribute("height", height);
         streaming = true;
       }
     },
   );
 }
+function takePicture() {
+  const context = canvas.getContext("2d");
+  if (width && height) {
+    canvas.width = video.videoWidth;
+    canvas.height = video.videoHeight;
 
+    canvas.style.width = width + 'px';
+    canvas.style.height = height + 'px';
+    context.drawImage(video, 0, 0);
+
+    const data = canvas.toDataURL("image/webp");
+    photo.setAttribute("src", data);
+  }
+  document.getElementById("video").style.display = "none";
+  document.getElementById("snap").style.display = "block";
+}
+
+document.getElementById("flip-cam").addEventListener("click", function (e) {
+  counter++;
+  if (counter % 2 == 0) {
+    navigator.mediaDevices
+      .getUserMedia({ video: { facingMode: 'user' }, audio: false })
+      .then((stream) => {
+        video.srcObject = stream;
+        video.play();
+      })
+  }
+  else {
+    navigator.mediaDevices
+      .getUserMedia({ video: { facingMode: 'environment' }, audio: false })
+      .then((stream) => {
+        video.srcObject = stream;
+        video.play();
+      })
+  }
+})
+
+document.getElementById("capture").addEventListener("click", function (e) {
+  e.preventDefault();
+  takePicture();
+});
 window.addEventListener("load", startup, false);
