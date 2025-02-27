91126e2b6acb9a1af6ab0c1da0a485a57fff3b85
Tejxsdev <danduhman7@gmail.com>
2025-02-27 14:56:04 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,34 @@
+const width = screen.width/4; 
+let height = 0; 
+
+let streaming = false;
+
+let video = null;
+
+function startup() {
+  video = document.getElementById("video");
+  navigator.mediaDevices
+    .getUserMedia({ video: true, audio: false })
+    .then((stream) => {
+      video.srcObject = stream;
+      video.play();
+    })
+    .catch((err) => {
+      console.error(`An error occurred: ${err}`);
+    });
+
+  video.addEventListener(
+    "canplay",
+    function(e) {
+      if (!streaming) {
+        height = video.videoHeight / (video.videoWidth / width);
+        video.set
+        video.setAttribute("width", width);
+        video.setAttribute("height", height);
+        streaming = true;
+      }
+    },
+  );
+}
+
+window.addEventListener("load", startup, false);
